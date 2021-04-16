e17902516387f5c83d16345e12ad246149a0ba0b
selsmie <simonelsmie@hotmail.com>
2021-04-16 14:02:21 +0100

    initial commit

---
+++ src/main/java/com/example/budgetawaydb/BudgetawayDbApplication.java
@@ -0,0 +1,13 @@
+package com.example.budgetawaydb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BudgetawayDbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BudgetawayDbApplication.class, args);
+	}
+
+}
