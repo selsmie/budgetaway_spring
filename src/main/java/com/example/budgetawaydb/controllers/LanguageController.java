ed3cb91961029f0cf144409c3c178360687bb69b
JamesSBeedle <jamessbeedle@gmail.com>
2021-04-21 15:49:11 +0100

    (eastern) Punjab changed to Eastern Punjab"

--- src/main/java/com/example/budgetawaydb/controllers/LanguageController.java
+++ src/main/java/com/example/budgetawaydb/controllers/LanguageController.java
@@ -1,12 +1,12 @@
 package com.example.budgetawaydb.controllers;
 
+import com.example.budgetawaydb.models.Flight;
 import com.example.budgetawaydb.models.Language;
 import com.example.budgetawaydb.repositories.LanguageRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -19,5 +19,13 @@ public class LanguageController {
     @GetMapping(value = "/languages")
     public ResponseEntity<List<Language>> getUniqueLanguages(){
         return new ResponseEntity<>(languageRepository.findAll(), HttpStatus.OK);
+    }
+
+    @PutMapping(value = "/languages/{id}")
+    public ResponseEntity<Language> putFlight(@RequestBody Language language, @PathVariable Long id) {
+        Language foundLanguage = languageRepository.findById(id).get();
+        foundLanguage.setName(language.getName());
+        languageRepository.save(foundLanguage);
+        return new ResponseEntity<>(foundLanguage, HttpStatus.OK);
     }
 }
